a82840815f780dc8402a488c78d2e31eae206b9e
saraang <saraangsk@gmail.com>
2019-08-01 12:42:24 +0000

    Bean added

--- src/main/java/guru/springframework/controllers/CustomController.java
+++ src/main/java/guru/springframework/controllers/CustomController.java
@@ -2,6 +2,7 @@ package guru.springframework.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import guru.springframework.controllers.HelloWorldBean;
 
 @RestController
 public class CustomController {
@@ -9,5 +10,10 @@ public class CustomController {
     @GetMapping(value = "/hello-world")
     public String helloWorld(){
         return "Hello World...!";
+    }
+
+    @GetMapping(value = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
     }
 }
