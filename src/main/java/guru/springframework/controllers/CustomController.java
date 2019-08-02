441fbed276c60ee3bb141879242777907420c03f
saraang <saraangsk@gmail.com>
2019-08-02 07:07:26 +0000

    new bean added with path-variable

--- src/main/java/guru/springframework/controllers/CustomController.java
+++ src/main/java/guru/springframework/controllers/CustomController.java
@@ -2,6 +2,8 @@ package guru.springframework.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import guru.springframework.controllers.HelloWorldBean;
 
 @RestController
@@ -15,5 +17,11 @@ public class CustomController {
     @GetMapping(value = "/hello-world-bean")
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(value = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean1(@PathVariable String name){
+        System.out.println("Name : "+name);
+        return new HelloWorldBean(String.format("Hello World, %s",name));
     }
 }
